eeeaf84a7cc6f4a80bbab33244efa3adc3883626
dongqn <36226384+dongqn@users.noreply.github.com>
2018-02-09 09:24:13 +0700

    Add files via upload

---
+++ stratum/algos/Lyra2z330.h
@@ -0,0 +1,14 @@
+#ifndef LYRA2Z330_H
+#define LYRA2Z330_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void lyra2z330_hash(const char* input, char* output, uint32_t 330);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
