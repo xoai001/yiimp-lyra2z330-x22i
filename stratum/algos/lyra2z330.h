30fd90386495056fe90d0c8f1c0395dd1cdd9c74
dongqn <36226384+dongqn@users.noreply.github.com>
2018-02-09 22:17:35 +0700

    Update lyra2z330.h

--- stratum/algos/lyra2z330.h
+++ stratum/algos/lyra2z330.h
@@ -2,17 +2,16 @@
 #define LYRA2Z330_H
 
 #ifdef __cplusplus
-
 extern "C" {
-
 #endif
 
-void lyra2z330_hash(const char* input, char* output, uint32_t height);
+#include <stdint.h>
+
+void lyra2z330_hash(const char* input, char* output, uint32_t len);
+
 
 #ifdef __cplusplus
-
 }
-
 #endif
 
 #endif
